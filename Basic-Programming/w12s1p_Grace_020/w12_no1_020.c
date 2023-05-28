0db8a263dfab18d73f63ad154ec5b91893596541
Grace Agnes Kesya <133007888+gkesya@users.noreply.github.com>
2023-05-28 16:12:16 +0700

    Create w12_no1_020.c

---
+++ Basic-Programming/w12s1p_Grace_020/w12_no1_020.c
@@ -0,0 +1,19 @@
+/*Nama 		: Grace Agnes Kesya
+NIM			: 13320020
+Deskripsi	: Program ini dapat mencetak keluaran
+*/
+
+#include <stdio.h>
+
+int main (){
+	int x = 10, y = 5; 
+	
+		printf("x = %d, y = %d\n", x,y);
+		printf("y+3 = %d\n", y+3);
+		printf("y-2 = %d\n", y-2);
+		printf("y*5 = %d\n", y*5);
+		printf("x/y = %d\n", x/y);
+		printf("x%y = %d\n", x%y);
+		
+	return 0;
+}
