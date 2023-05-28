bf9d07c81a7e74419f42d7da1aa9a37dc9eea701
Grace Agnes Kesya <133007888+gkesya@users.noreply.github.com>
2023-05-28 15:42:08 +0700

    Add files via upload

---
+++ Basic-Programming/01_020.c
@@ -0,0 +1,24 @@
+/*
+Nama 		: Grace Agnes Kesya 
+NIM			: 13320020
+Deskripsi	: Menentukan nilai cd 
+Tanggal		: 19 September 2020
+*/
+#include <stdio.h>
+
+int main ()
+{
+	int x = 213;
+	int y = 421;
+	int z;
+	printf("%d+4=421\n", x);
+	printf("%d=421\n", x);
+	
+	z =  y - 4;
+	printf("\t=%d\n\n", z);
+	printf("Jadi hasil 693cd=%dcd x 3\n",z);
+	printf("\t%d\n\n", z*3);
+	
+	return 0;
+	
+}
---
+++ Basic-Programming/02_020.c
@@ -0,0 +1,20 @@
+/*
+Nama 		: Grace Agnes Kesya 
+NIM			: 13320020
+Deskripsi	: Menentukan nilai cd 
+Tanggal		: 19 September 2020
+*/
+#include <stdio.h>
+
+int main ()
+{
+	int a = 2;
+	int b = 3;
+	int n = 20;
+	int x;
+	printf("Deret bilangan adalah 2, 5, 8, 11,....\nAgnes Friska ingin mengetahui suku ke 20.\n");
+	
+	x = 2+(n-1)*b;
+	printf("Maka hasilnya adalah :%d", x);
+	return 0;
+}
